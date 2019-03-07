292b0f92af787b3634e231ac4c8ee09e6d14450a
liruxiao <liruxiao@163.com>
2019-03-07 22:17:47 +0800

    [+]项目初始化

---
+++ application/admin/controller/Index.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\admin\controller;
+
+class Index {
+
+    public function index() {
+
+        $temp = config();
+
+        dump($temp);
+    }
+
+}
---
+++ application/common/controller/Index.php
@@ -0,0 +1,10 @@
+<?php
+namespace app\common\controller;
+
+class Index
+{
+    public function index()
+    {
+        echo  '我是common模块index控制器index方法';
+    }
+}
